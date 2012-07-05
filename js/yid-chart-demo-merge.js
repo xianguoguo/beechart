9966735bb5dff544b33348563f60d1af958bc566
sjpsega <ssjjpp520@126.com>
2012-07-05 19:29:02 +0800

    commit

---
+++ js/yid-chart-demo-merge.js
@@ -0,0 +1,16 @@
+/*merge start*/
+(function(){
+	ImportJavscript = {
+		url:function(url){
+			document.write("<script type=\"text/javascript\" src=\""+url+"\"></scr"+"ipt>");
+		}
+	}
+})();
+/*merge end*/
+ImportJavscript.url("http://style.china.alibaba.com/js/lib/fdev-v4/core/fdev-min.js");
+ImportJavscript.url("../js/widget-min.js");
+ImportJavscript.url("../js/flash-min.js");
+ImportJavscript.url("../js/flash-chart-min.js");
+ImportJavscript.url("../js/jquery.easing.1.2.js");
+ImportJavscript.url("../js/jquery.anythingslider.js");
+ImportJavscript.url("../js/yid-chart-demo.js");
