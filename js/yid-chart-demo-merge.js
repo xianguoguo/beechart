878ca9c1ecae8008ba98611bc2dc764a698498cd
sjpsega <ssjjpp520@126.com>
2012-07-20 16:04:37 +0800

    commit

--- js/yid-chart-demo-merge.js
+++ js/yid-chart-demo-merge.js
@@ -8,9 +8,9 @@
 })();
 /*merge end*/
 ImportJavscript.url("http://style.china.alibaba.com/js/lib/fdev-v4/core/fdev-min.js");
-ImportJavscript.url("../js/widget-min.js");
-ImportJavscript.url("../js/flash-min.js");
-ImportJavscript.url("../js/flash-chart-min.js");
-ImportJavscript.url("../js/jquery.easing.1.2.js");
-ImportJavscript.url("../js/jquery.anythingslider.js");
-ImportJavscript.url("../js/yid-chart-demo.js");
+ImportJavscript.url("js/widget-min.js");
+ImportJavscript.url("js/flash-min.js");
+ImportJavscript.url("js/flash-chart-min.js");
+ImportJavscript.url("js/jquery.easing.1.2.js");
+ImportJavscript.url("js/jquery.anythingslider.js");
+ImportJavscript.url("js/yid-chart-demo.js");
